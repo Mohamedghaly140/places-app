af2ee15cf7ca0b9760132fe2280dcdee9e6c4ab1
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-12-29 11:40:24 +0200

    modify

--- app.js
+++ app.js
@@ -33,7 +33,10 @@ const usersRoutes = require('./routes/usersRoutes');
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE OPTIONS');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'OPTIONS, GET, POST, PUT, DELETE'
+	);
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	next();
 });
